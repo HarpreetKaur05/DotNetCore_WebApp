e386c2804893968ab5ed40b41a189c4aa3100704
HarpreetKaur05 <70134172+HarpreetKaur05@users.noreply.github.com>
2020-08-27 11:23:36 +0530

    Initial Commit
    Initial commit

---
+++ Models/Doctor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore.Models
+{
+    public class Doctor
+    {
+        [Key]
+        public int DoctorId { get; set; }
+        [Required]
+        public string DoctorName { get; set; }
+        public string DoctorAddress { get; set; }
+        public int DoctorAge { get; set; }
+        public string RMPNumber { get; set; }
+        public string HighestQualification { get; set; }
+        public string Speciality { get; set; }
+
+
+    }
+}
---
+++ StructureMap/StructureMappingService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCCore.StructureMap
+{   
+    public class StructureMappingService : IMessagingService
+    {
+        public string GetMessage()
+        {
+            return "Hello from Structure Map!!!";
+        }
+
+    }
+}
